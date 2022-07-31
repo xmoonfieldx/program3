ecb2e1e543894546690303d4707f2f052a9be505
Manjeera Reddy <61614013+xmoonfieldx@users.noreply.github.com>
2022-07-31 11:52:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/program3/Message.java
@@ -0,0 +1,11 @@
+package com.example.program3;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
+
